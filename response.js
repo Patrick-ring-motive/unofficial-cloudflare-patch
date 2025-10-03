2c7ca84cf85a3e17962a6ea0627d572e20e32b87
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2025-10-02 19:21:05 -0500

    Update response.js

--- response.js
+++ response.js
@@ -1,22 +1,20 @@
-
-
-(()=>{
+(() => {
     const $Response = globalThis.Response;
-    globalThis.Response = class Response extends $Response{
-        constructor(...args){
-            try{
-                if(/^(101|204|205|304)$/.test(args?.[1]?.status)){
+    globalThis.Response = class Response extends $Response {
+        constructor(...args) {
+            try {
+                if (/^(101|204|205|304)$/.test(args?.[1]?.status)) {
                     console.warn('Trying to give a body to in compatible response code 101|204|205|304; body ignored');
-                    (args??[])[0] = null;
-                    delete (args??[])[1].body;
+                    (args ?? [])[0] = null;
+                    delete(args ?? [])[1].body;
                 }
                 return super(...args);
-            }catch(e){
-                console.warn(e,...args);
-              super(Object.getOwnPropertyNames(e??{}).map(x=>`${x} : ${e[x]}`).join('\n'),{
-          status : 569,
-          statusText:e?.message
-        });
+            } catch (e) {
+                console.warn(e, ...args);
+                super(Object.getOwnPropertyNames(e ?? {}).map(x => `${x} : ${e[x]}`).join('\n'), {
+                    status: 569,
+                    statusText: e?.message
+                });
             }
         }
     }
