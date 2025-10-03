f063f42fd2200bcae8451c4b9c9a82e627855450
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2025-10-02 20:34:35 -0500

    Update fetch.js

--- fetch.js
+++ fetch.js
@@ -1,15 +1,24 @@
-(()=>{
+(() => {
     const $fetch = globalThis.fetch;
-    globalThis.fetch = Object.setPrototypeOf(async function fetch(...args){
-      let request,response;
-      try{
-        return await $fetch(...args);
-      }catch(e){
-        console.warn(e,...arguments);
-        return new Response(Object.getOwnPropertyNames(e??{}).map(x=>`${x} : ${e[x]}`).join(''),{
-          status : 569,
-          statusText:e?.message
-        });
-      }
-    },$fetch);
+    globalThis.fetch = Object.setPrototypeOf(async function fetch(...args) {
+        let response;
+        try {
+            response = $fetch(...args);
+        } catch (e) {
+            console.warn(e, ...args);
+            return new Response(Object.getOwnPropertyNames(e ?? {}).map(x => `${x} : ${e[x]}`).join('\n'), {
+                status: 400,
+                statusText: `400 Bad Request ${e?.message}`
+            });
+        }
+        try {
+            return await response;
+        } catch (e) {
+            console.warn(e, ...args);
+            return new Response(Object.getOwnPropertyNames(e ?? {}).map(x => `${x} : ${e[x]}`).join('\n'), {
+                status: 500,
+                statusText: `500 Internal Server Error ${e?.message}`
+            });
+        }
+    }, $fetch);
 })();
