d97f0b35ecfd9101cf6c2bf0143fea19315f1d23
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2025-10-02 21:22:50 -0500

    Update cache.js

--- cache.js
+++ cache.js
@@ -1,1 +1,20 @@
-
+(()=>{
+  const $match = Cache.prototype.match;
+  Cache.prototype.match = Object.setPrototypeOf(async function match(...args){
+    try{
+      return await $match.apply(this,args);
+    }catch(e){
+      console.warn(e,this,...args);
+    }
+  },$match);
+})();
+(()=>{
+  const $match = Cache.prototype.match;
+  Cache.prototype.match = Object.setPrototypeOf(async function match(...args){
+    try{
+      return await $match.apply(this,args);
+    }catch(e){
+      console.warn(e,this,...args);
+    }
+  },$match);
+})();
