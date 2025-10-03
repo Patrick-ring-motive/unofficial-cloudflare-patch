bc5ac13c60cdabea93be039eaf45324576a93140
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2025-10-02 21:38:03 -0500

    Update cache.js

--- cache.js
+++ cache.js
@@ -39,3 +39,35 @@
     }
   },$addAll);
 })();
+(()=>{
+  const $put = Cache.prototype.put;
+  Cache.prototype.put = Object.setPrototypeOf(async function put(...args){
+    try{
+      return await $put.apply(this,args);
+    }catch(e){
+      console.warn(e,this,...args);
+    }
+  },$put);
+})();
+(()=>{
+  const _delete = Cache.prototype.delete;
+  Cache.prototype.delete = Object.setPrototypeOf(async function $delete(...args){
+    try{
+      return await _delete.apply(this,args);
+    }catch(e){
+      console.warn(e,this,...args);
+      return false
+    }
+  },_delete);
+})();
+(()=>{
+  const $keys = Cache.prototype.keys;
+  Cache.prototype.keys = Object.setPrototypeOf(async function keys(...args){
+    try{
+      return await $keys.apply(this,args);
+    }catch(e){
+      console.warn(e,this,...args);
+      return [];
+    }
+  },$keys);
+})();
