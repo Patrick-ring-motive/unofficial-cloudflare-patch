ab4fb2950e0ad830e075e46fbe312d70bc6a155c
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2025-10-14 20:31:16 -0500

    Update regexp.js

--- regexp.js
+++ regexp.js
@@ -1,3 +1,5 @@
+
+//original
 (()=>{
   const _RegExp = globalThis.RegExp;
   globalThis.RegExp = class RegExp extends _RegExp{
@@ -10,4 +12,41 @@
       }
     }
   };
+})();
+
+// refactor
+(()=>{
+  const Extends = (thisClass, superClass)=>{
+    try{
+      Object.setPrototypeOf(thisClass, superClass);
+      Object.setPrototypeOf(
+        thisClass.prototype, 
+        superClass?.prototype ??
+        superClass?.constructor?.prototype ??
+        superClass
+      );
+    }catch(e){
+      console.warn(e, {thisClass, superClass});
+    }
+    return thisClass;
+  };
+  const _RegExp = globalThis.RegExp;
+  (()=>{
+  globalThis.RegExp = Extends(function RegExp(...args){
+      if(new.target){
+      try{
+        return Reflect.construct(_RegExp,args,new.target);
+      }catch(e){
+        console.warn(e,...args);
+        return Reflect.construct(_RegExp,[/$a^/],new.target);
+      }
+      }
+    try{
+        return _RegExp(...args);
+      }catch(e){
+        console.warn(e,...args);
+        return _RegExp(/$a^/);
+      } 
+    },_RegExp);
+})();
 })();
