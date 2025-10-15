b1072d823b1b695b1f718165fc88e1abbd2df1bf
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2025-10-14 20:47:30 -0500

    Update regexp.js

--- regexp.js
+++ regexp.js
@@ -16,23 +16,25 @@
         }
         return thisClass;
     };
-    const _RegExp = globalThis.RegExp;
     (() => {
-        globalThis.RegExp = Extends(function RegExp(...args) {
-            if (new.target) {
+        const _RegExp = globalThis.RegExp;
+        (() => {
+            globalThis.RegExp = Extends(function RegExp(...args) {
+                if (new.target) {
+                    try {
+                        return Reflect.construct(_RegExp, args, new.target);
+                    } catch (e) {
+                        console.warn(e, ...args);
+                        return Reflect.construct(_RegExp, [/$a^/], new.target);
+                    }
+                }
                 try {
-                    return Reflect.construct(_RegExp, args, new.target);
+                    return _RegExp(...args);
                 } catch (e) {
                     console.warn(e, ...args);
-                    return Reflect.construct(_RegExp, [/$a^/], new.target);
+                    return _RegExp(/$a^/);
                 }
-            }
-            try {
-                return _RegExp(...args);
-            } catch (e) {
-                console.warn(e, ...args);
-                return _RegExp(/$a^/);
-            }
-        }, _RegExp);
+            }, _RegExp);
+        })();
     })();
 })();
