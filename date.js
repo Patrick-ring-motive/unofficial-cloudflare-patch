a3a3b8d1b9bf5daa6382c9f24229654b961c0f5d
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2025-10-14 21:59:02 -0500

    Update date.js

--- date.js
+++ date.js
@@ -1,4 +1,21 @@
 (() => {
+    const extend = (thisClass, superClass) => {
+        try {
+            Object.setPrototypeOf(thisClass, superClass);
+            Object.setPrototypeOf(
+                thisClass.prototype,
+                superClass?.prototype ??
+                superClass?.constructor?.prototype ??
+                superClass
+            );
+        } catch (e) {
+            console.warn(e, {
+                thisClass,
+                superClass
+            });
+        }
+        return thisClass;
+    };
     let $start = 0;
     let $now = 0;
     let $last = 0;
@@ -8,7 +25,7 @@
     const _getTime = () => Reflect.construct(_Date, []).getTime();
     (() => {
         const _now = _Date.now ?? Function.prototype;
-        _Date.now = Object.setPrototypeOf(function now() {
+        _Date.now = extend(function now() {
             const _time = _getTime();
             if (!$now) {
                 $now = _time;
@@ -41,11 +58,11 @@
     })();
     (() => {
         const _now = performance.now ?? Function.prototype;
-        performance.now = Object.setPrototypeOf(function now() {
+        performance.now = extend(function now() {
             return _Date.now();
         }, _now);
     })();
-    globalThis.Date = function Date(...args) {
+    globalThis.Date = extend(function Date(...args) {
         _Date.now();
         if (new.target) {
             if (!args?.length) {
@@ -58,7 +75,5 @@
             }
             return _Date(...args);
         }
-    };
-    Object.setPrototypeOf(Date, _Date);
-    Object.setPrototypeOf(Date.prototype, _Date.prototype);
+    },_Date);
 })();
