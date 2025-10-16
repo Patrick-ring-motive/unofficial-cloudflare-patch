79596d4e7ce4eb663178674804faf0a2b7aa46f7
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2025-10-16 13:43:59 -0500

    Create weak-cache-fetch.js

---
+++ weak-cache-fetch.js
@@ -0,0 +1,117 @@
+
+(() => {
+    const WeakRefMap = (() => {
+        const _weakRefMap = new Map();
+        return class WeakRefMap extends Map {
+            get(key) {
+                try {
+                    const ref = _weakRefMap.get(key);
+                    const value = ref?.deref?.();
+                    if (value === undefined) {
+                        _weakRefMap.delete(key);
+                    }
+                    return value;
+                } catch (e) {
+                    console.warn(e, key);
+                }
+            }
+            set(key, value) {
+                try {
+                    _weakRefMap.set(key, new WeakRef(value));
+                } catch (e) {
+                    console.warn(e, key, value);
+                }
+                return this;
+            }
+            delete(key) {
+                try {
+                    return _weakRefMap.delete(key);
+                } catch (e) {
+                    console.warn(e, key);
+                }
+            }
+            has(key) {
+                try {
+                    const value = _weakRefMap.get(key)?.deref?.();
+                    if (value === undefined) {
+                        _weakRefMap.delete(key);
+                        return false;
+                    }
+                    return true;
+                } catch (e) {
+                    console.warn(e, key);
+                    return false;
+                }
+            }
+        }
+    })();
+    const instanceOf = (x, y) => {
+        try {
+            return x instanceof y;
+        } catch {
+            return false;
+        }
+    };
+
+    const isValidResponse = x => (x?.status === 200 && !x?.bodyUsed && !x?.body?.locked) || x?.status === 304;
+    const isResponse = x => instanceOf(x, Response) || x?.constructor?.name == 'Response';
+    const isPromise = x => instanceOf(x, Promise) || x?.constructor?.name == 'Promise' || typeof x?.then === 'function';
+    globalThis.WeakCache = new WeakRefMap();
+    const $response = Symbol('*response');
+    const $fetch = Symbol('*fetch');
+    const parrayBuffer = async(presponse)=>{
+        if(isPromise(presponse)){
+            presponse = await presponse;
+        }
+        console.log(presponse);
+        return presponse?.clone?.()?.arrayBuffer?.() 
+        ?? presponse.slice();
+    };
+    globalThis[$fetch] = fetch;
+    globalThis.fetch = async function fetch() {
+        let request, response;
+        try {
+            request = new Request(...arguments);
+            if (request.method === 'GET') {
+                let cachedResponse = WeakCache.get(request.url);
+                if (cachedResponse) {
+                    request[$response] = cachedResponse;
+                    if (isPromise(cachedResponse)) {
+                        cachedResponse = await cachedResponse;
+                        if (isValidResponse(cachedResponse)) {
+                            WeakCache.set(request.url, cachedResponse);
+                        } else {
+                            WeakCache.delete(request.url);
+                        }
+                    }
+                    try {
+                        response = new Response(cachedResponse);
+                        response[$response] = cachedResponse;
+                    } catch {
+                        WeakCache.delete(request.url);
+                    }
+                    console.log('response from cache');
+                } else {
+                    const presponse = globalThis[$fetch](...arguments);
+                    WeakCache.set(request.url, parrayBuffer(presponse));
+                    response = await presponse;
+                    if (response.status === 200 && !response.bodyUsed) {
+                        WeakCache.set(request.url, parrayBuffer(response));
+                    } else {
+                        WeakCache.delete(request.url);
+                    }
+                }
+            }
+            if (!isResponse(response)) {
+                response = await globalThis[$fetch](...arguments);
+            }
+            return response;
+        } catch (e) {
+            WeakCache.delete(request.url);
+            return new Response(Object.getOwnPropertyNames(e).map(x => `${x} : ${e[x]}`).join('\n'), {
+                status: 500,
+                statusText: `500 Internal Server Error ${e?.message}`.trim()
+            });
+        }
+    };
+})();
